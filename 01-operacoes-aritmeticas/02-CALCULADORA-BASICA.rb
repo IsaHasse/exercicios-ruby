768b0dbb2d1e5b3c3b7e0943505fc5ab19582654
Isabelle Hasse <isabelle-hasse@hotmail.com>
2023-01-24 21:44:47 -0300

    Exercicio Calculadora Básica

---
+++ 01-operacoes-aritmeticas/02-CALCULADORA-BASICA.rb
@@ -0,0 +1,23 @@
+print "Informe o primeiro número inteiro: "
+
+num1 = gets.chomp
+
+num1 = num1.to_i
+
+print "Informe o segundo número inteiro: "
+
+num2 = gets.chomp
+
+num2 = num2.to_i
+
+puts "\nSoma = #{num1 + num2}"
+
+puts "Subtração = #{num1 - num2}"
+
+puts "Multiplicação = #{num1 * num2}"
+
+puts "Divisão = #{num1 / num2}"
+
+puts "Resto = #{num1 % num2}"
+
+puts "Potenciação = #{num1 ** num2}"
