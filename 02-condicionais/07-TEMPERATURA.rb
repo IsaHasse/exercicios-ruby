befaba5ab69bac8a7c19004c95a062608a809899
Isabelle Hasse <isabelle-hasse@hotmail.com>
2023-05-02 22:18:25 -0300

    Exercício Temperatura

---
+++ 02-condicionais/07-TEMPERATURA.rb
@@ -0,0 +1,20 @@
+print "Informe a escala de temperatura (C/F): "
+
+escala = gets.chomp
+
+print "Informe a temperatura: "
+
+temp = gets.chomp
+
+temp = temp.to_f
+
+if (escala == 'C'|| escala == 'c')
+  print "\n#{temp} C = #{((temp * 1.8) + 32).ceil(2)} F"
+
+elsif (escala == 'F' || escala == 'f')
+  print "\n#{temp} F = #{((temp - 32) / 1.8).ceil(2)} C"
+
+else 
+  print "\nInforme uma escala válida!"
+
+end
